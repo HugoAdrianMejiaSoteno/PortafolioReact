c000aa4ed8da9f4c9c08e84136dd6204e2f606ca
Hugo Adrian Mejia Soteno <mejiasot57@gmail.com>
2023-06-08 16:36:38 -0600

    Cambio HashRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 /*Estamos usando la libreria de react-icons y react-router-dom y framer-motion*/
 import Header from './Componentes/Header/Header';
-import { BrowserRouter as Router} from 'react-router-dom'; 
+import { HashRouter  as Router} from 'react-router-dom'; 
 import './App.css';
 import AnimatedRoutes from './Componentes/AnimatedRoutes';
 //Para las animaciones entre paginamas vamos a utilizar la libreria de framer motion de npm, esta nos permite anadir transisiones entre paginas utilizando reaact router
 
 //Dentro ded AnimatedRoutes esta toda la logica de las rutas, solo que lo mandamos a otro archivo diferente en vez de tener aqui todo junto para poder implementar la libreria de framer-motion ya que no se podia hacer directamente por el tema de useLocation
 
+//  Como lo voy a subir a github necesito convertirlo a estatico por eso instalamos una libreria githubpages
+//Ademas cree una homepage y modifique unas cosas en e packgae 
+//tambien voy a cambiar el BrowserRouter as Router por  HashRouter el cual nos ayuda a reinciiar las rutas cuando entremos a un link y volvamos a la pagina
 function App() {
   return (
     <Router>
